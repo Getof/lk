da05fb215717ab553ed9a7a1f8bf4cda603eb1b2
getof <infogetof@gmail.com>
2020-04-26 18:19:50 +0700

    up

---
+++ public/front/js/register.js
@@ -0,0 +1,10 @@
+let type = "student";
+$(".register-modal__type-block").on("click", function() {
+	$(".register-modal__type-block").each(function(){
+	     $(this).removeClass('register-modal__type-checked');
+	})
+	$(this).addClass("register-modal__type-checked");
+	type = $(this).attr('data-type');
+	if (type === "parent") $('.register-modal__form-input__invite').css("display","block");
+	else $('.register-modal__form-input__invite').css("display","none");
+});
